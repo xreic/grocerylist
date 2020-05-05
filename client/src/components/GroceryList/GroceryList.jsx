7b0fef33d90f93ca48fca9303a6bc77c7d83f5c9
Eric Lau <ericlaucmo@gmail.com>
2020-05-04 17:25:54 -0700

    Converted List to SimpleGrid

--- client/src/components/GroceryList/GroceryList.jsx
+++ client/src/components/GroceryList/GroceryList.jsx
@@ -2,36 +2,29 @@
 import React from 'react';
 
 // Styled-Components + ChakraUI
-import { List, ListItem } from '@chakra-ui/core';
+import { SimpleGrid, Heading } from '@chakra-ui/core';
 
 const GroceryList = () => {
   return (
-    <List styleType="disc">
-      <ListItem>
-        <span> frozen pizza </span>
-        <span> 5 </span>
-      </ListItem>
-      <ListItem>
-        <span> noosa yogurt </span>
-        <span> 10 </span>
-      </ListItem>
-      <ListItem>
-        <span> wine </span>
-        <span> 2 </span>
-      </ListItem>
-      <ListItem>
-        <span> iced coffe </span>
-        <span> 1 </span>
-      </ListItem>
-      <ListItem>
-        <span> a </span>
-        <span> 1 </span>
-      </ListItem>
-      <ListItem>
-        <span> pizza </span>
-        <span> 1 </span>
-      </ListItem>
-    </List>
+    <SimpleGrid columns="2">
+      <Heading as="h3" size="lg">
+        Item
+      </Heading>
+      <Heading as="h3" size="lg">
+        Quantity
+      </Heading>
+
+      <span> frozen pizza </span>
+      <span> 5 </span>
+      <span> noosa yogurt </span>
+      <span> 10 </span>
+      <span> wine </span>
+      <span> 2 </span>
+      <span> iced coffee </span>
+      <span> 1 </span>
+      <span> pizza </span>
+      <span> 1 </span>
+    </SimpleGrid>
   );
 };
 
